20a46a24a85374f0defe9e5b59c36015acae8201
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-01-10 07:36:31 +0700

    change port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const PORT = process.env.PORT || 5000
 
 // if you want to use another directory instead of views
 // app.set('views', path.join(__dirname, '/pages'));
 app.set('view engine', 'ejs') 
 app.get('/', (req, res) => res.render('index'))
 
-app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 3000!'))
