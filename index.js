6e63134f01ef2b222992a063da2da23ce76dcda0
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-01-10 07:40:19 +0700

    redeploy

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ const PORT = process.env.PORT || 5000
 // app.set('views', path.join(__dirname, '/pages'));
 app.set('view engine', 'ejs') 
 app.get('/', (req, res) => res.render('index'))
-
+//test
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
