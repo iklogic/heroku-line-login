990b6161d9a06ff8ce2b753b4b9fed8e7857922b
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-01-10 22:30:50 +0700

    line login

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const PORT = process.env.PORT || 5000
-
+const PORT = process.env.PORT || 3000
+const line_login = require("line-login");
+ 
+const login = new line_login({
+    channel_id: process.env.LINE_LOGIN_CHANNEL_ID,
+    channel_secret: process.env.LINE_LOGIN_CHANNEL_SECRET,
+    callback_url: process.env.LINE_LOGIN_CALLBACK_URL,
+    scope: "openid profile",
+    prompt: "consent",
+    bot_prompt: "normal"
+});
 // if you want to use another directory instead of views
 // app.set('views', path.join(__dirname, '/pages'));
 app.set('view engine', 'ejs') 
-app.get('/', (req, res) => res.render('index'))
+// app.get('/', (req, res) => res.render('index'))
 
 app.listen(PORT, () => console.log('Example app listening on port 3000!'))
+
+
+app.use("/", login.auth());
+ 
+// Specify the path you want to wait for the callback from LINE authorization endpoint.
+app.use("/callback", login.callback((req, res, next, token_response) => {
+    // Success callback
+    res.json(token_response);
+},(req, res, next, error) => {
+    // Failure callback
+    res.status(400).json(error);
+}));
