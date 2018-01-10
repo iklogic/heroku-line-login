db0623d406c02e6b98301eb1d362190a33df1b80
Narongsak Keawmanee <klogic@hotmail.co.th>
2018-01-10 07:06:58 +0700

    create simple express

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+// if you want to use another directory instead of views
+// app.set('views', path.join(__dirname, '/pages'));
+app.set('view engine', 'ejs') 
+app.get('/', (req, res) => res.render('index'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
